cb904d5c775f17c4bc8647eac21e61a73b3efbec
Ivan Mladenov <imladenov@icloud.com>
2020-11-02 13:56:38 -0600

    Create floofy.ml

---
+++ floofy.ml
@@ -0,0 +1,20 @@
+let _ = 
+  try
+    if Array.length Sys.argv < 2 then (
+      print_endline "Bork, give more file.";
+      exit 1
+    ) else (
+      if Array.lenght Sys.argv > 2 then (
+        print_endline "Bork, give less file."
+        exit 1
+      ) else ( 
+        let x = open_in Sys.argv.(1) in 
+        let lexbuf = Lexing.from_channel x in
+        while true do
+          let result = Fparser.main Flexer.token lexbuf in
+          print_int result; print_newline(); flush stdout
+        done
+      )
+    );
+  with Flexer.Eof ->
+    exit 0
